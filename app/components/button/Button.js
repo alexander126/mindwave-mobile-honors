1a8ea38652def3143bfd2bb659108b5ab867adb5
Aleksandar Kalapsazov <aleks@gearedapp.co.uk>
2019-01-15 14:41:32 +0000

    ➕Add MindWave library to the project

--- app/components/button/Button.js
+++ app/components/button/Button.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { TouchableOpacity, Text } from 'react-native';
+import MindWaveMobile from 'react-native-mindwave-mobile';
 
 import styles from './styles';
 
 export default class Button extends Component {
+  componentDidMount() {
+    const mwm = new MindWaveMobile();
+    mwm.scan();
+    mwm.onFoundDevice(device => {
+      console.log(device);
+    });
+  }
   render() {
     return (
       <TouchableOpacity style={styles.buttonContainer}>
